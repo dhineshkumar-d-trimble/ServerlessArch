0ab22f3b0f63b8d23ad5c29be02136a31d375e8d
dhineshkumar-d-trimble <dhineshkumar_d@trimble.com>
2025-02-11 14:53:47 +0530

    Pipeline for SAM deployment

--- sam-arch/Authorizer.py
+++ sam-arch/Authorizer.py
@@ -1,14 +1,30 @@
 import boto3
+import hmac
+import hashlib
+import base64
 
-client = boto3.client('cognito-idp', region_name='ap-south-1')
+# Replace these values with your own
+username = 'dhineshkumard'
+password = 'Dhinesh007#'
+client_id = '7g7esub8aq2ntu05nu1eh7err4'
+client_secret = '1b1qd3mudm3dfa8c23dafm8h0oeh5jbur0i929q2kaug6p1ansaf'
+region = 'ap-south-1'
+
+def get_secret_hash(username, client_id, client_secret):
+    message = username + client_id
+    dig = hmac.new(client_secret.encode('utf-8'), message.encode('utf-8'), hashlib.sha256).digest()
+    return base64.b64encode(dig).decode()
+
+client = boto3.client('cognito-idp', region_name=region)
 
 response = client.initiate_auth(
     AuthFlow='USER_PASSWORD_AUTH',
     AuthParameters={
-        'USERNAME': 'b1135dda-a061-70c2-bb19-91310871c99b',
-        'PASSWORD': 'Dhinesh007#'
+        'USERNAME': username,
+        'PASSWORD': password,
+        'SECRET_HASH': get_secret_hash(username, client_id, client_secret)
     },
-    ClientId='your-cognito-app-client-id'
+    ClientId=client_id
 )
 
 id_token = response['AuthenticationResult']['IdToken']
